7c282556a4cf86b9d2283dbc5267890e9237c90e
NIshant <nishant@nishant-laptop.(none)>
2011-03-07 20:59:21 +0530

    argument switcher - higher order function

--- src/Sample.clj
+++ src/Sample.clj
@@ -49,3 +49,11 @@
 (println (str "Sum of numbers <= 3 is" (add-numbers 3)))
 (println (str "sum of numbers <= 10 is" (add-numbers 10)))
 (println (str "sum of numbers <= 10000 is " (add-numbers 10000)))
+
+;argument switcher
+(defn arg-switch
+    "calls a function with 2 arguments in all possible orders"
+    [fun arg1 arg2]
+    (list (fun arg1 arg2) (fun arg2 arg1)))
+(println (str "Calling / with 2 arguments and switching them" (arg-switch / 2 3)))
+(println (str "Calling str with 2 arguments and switching them" (arg-switch str " Hello " " nishant ")))
